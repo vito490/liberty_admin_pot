61f4418af4df03918823a41835da8014eb75df2e
Kevin Postreich <kevinlp@us.ibm.com>
2023-03-21 15:51:21 -0400

    add new jmeter scripts

--- lab-scripts/pbwLoadTest.sh
+++ lab-scripts/pbwLoadTest.sh
@@ -3,4 +3,4 @@ LAB_FILES=/home/techzone/liberty_admin_pot
 JMETER_HOME="/home/techzone/apache-jmeter-5.5/"
 SCRIPTS_DIR=$LAB_FILES/lab-scripts
 
-$JMETER_HOME/bin/jmeter -n -t $SCRIPTS_DIR/scriptArtifacts/PBW_Test_Plan.jmx -l $LAB_HOME/Downloads/pbwLoadTest.cvs
+$JMETER_HOME/bin/jmeter -n -t $SCRIPTS_DIR/scriptArtifacts/PBW_Test_Plan_https.jmx -l $LAB_HOME/Downloads/pbwLoadTest.cvs
