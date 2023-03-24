68213ec7085819819e2dece35c0c6fc937c616ca
Kevin Postreich <kevinlp@us.ibm.com>
2023-03-24 16:59:17 -0400

    update scripts

--- lab-scripts/instanaAddServerTags.sh
+++ lab-scripts/instanaAddServerTags.sh
@@ -1,3 +1,25 @@
+
+
+echo ""
+echo "-------------------------------------"
+echo "Running instanaAddSererTags.sh script"
+echo "-------------------------------------" 
+echo "" 
 sed -i '1iINSTANA_SERVICE_NAME=appServer1' /home/techzone/lab-work/liberty-staging/22.0.0.8-appServer1/wlp/usr/servers/appServer1/server.env
-​
+
+echo ""
+echo "---> Added'1iINSTANA_SERVICE_NAME=appServer1' to appServer1's server.env file"
+echo "" 
+
+
 sshpass -p "IBMDem0s!" ssh server1.gym.lan 'sed -i "1iINSTANA_SERVICE_NAME=appServer2" /opt/IBM/liberty-staging/22.0.0.8-appServer2/wlp/usr/servers/appServer2/server.env'
+
+
+echo ""
+echo "---> Added '1iINSTANA_SERVICE_NAME=appServer2' to appServer2's server.env file"
+echo "" 
+
+
+echo ""
+echo "instanaAddSererTags.sh completed"
+echo "" 
