c82b89d390ecc95e4709875234f3184ca542a8d3
Kevin Postreich <kevinlp@us.ibm.com>
2023-03-24 17:39:47 -0400

    add script to update instana config

--- lab-scripts/updateInstanaAgentConfigration.sh
+++
@@ -1,5 +0,0 @@
-sudo systemctl stop instana-agent
-sleep 5
-sudo cp -f /home/techzone/liberty_admin_pot/lab-scripts/scriptArtifacts/instana-configuration.yaml /opt/instana/agent/etc/instana/configuration.yaml
-sleep 5
-sudo systemctl start instana-agent
